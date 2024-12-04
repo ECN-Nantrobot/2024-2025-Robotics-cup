5b2c9508b8b09e4ce41557b4e60ebb7e25f82f29
Baschi464 <alessandrobaschiera26@gmail.com>
2024-12-04 20:23:42 +0100

    Update localization.cpp
    Added the implementation of the function that computes the motor angles required to reach the target position

--- localization/src/localization.cpp
+++ localization/src/localization.cpp
@@ -20,4 +20,26 @@ vector<double> Pose::updatePose(double microstep_left, double microstep_right, d
     return {x,y,theta};
 }
 
+    // Compute wheel angles based on target position
+        // kp = proportional gain of the controller
+std::pair<double, double> computeWheelAngles(double target_x, double target_y, double r, double L, double kp) {
+    // Calculate the Euclidean distance to the target
+    double distance = sqrt(pow(target_x - x, 2) + pow(target_y - y, 2));
+    
+    // Calculate the angle error to the target
+    double target_angle = atan2(target_y - y, target_x - x);
+    double angle_error = target_angle - theta;
+
+    // Normalize angle_error to [-pi, pi]
+    while (angle_error > M_PI) angle_error -= 2 * M_PI;
+    while (angle_error < -M_PI) angle_error += 2 * M_PI;
+
+    // Compute the wheel control matrix elements
+    double thetaL = (kp / r) * distance + (kp * L / r) * angle_error;
+    double thetaR = (kp / r) * distance - (kp * L / r) * angle_error;
+
+    // Return the pair of wheel angles
+    return std::make_pair(thetaL, thetaR);
+}
+
 
